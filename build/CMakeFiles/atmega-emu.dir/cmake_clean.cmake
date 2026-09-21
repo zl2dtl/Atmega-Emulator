6d0f6be419eec309ee4bb36a6131fa65106501c5
zl2dtl <radiodanzl2dtl@gmail.com>
2026-09-22 09:50:20 +1200

    Initialised my repo, testing build environment

---
+++ build/CMakeFiles/atmega-emu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/atmega-emu.dir/src/main.cpp.o"
+  "CMakeFiles/atmega-emu.dir/src/main.cpp.o.d"
+  "atmega-emu"
+  "atmega-emu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/atmega-emu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "ATmega 328P Emulator\n";
+    std::cout << "--------------------\n";
+
+    return 0;
+
+}
