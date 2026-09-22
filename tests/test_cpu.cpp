1e91ffe82fbba1f3648b0686e62c3e83a4517a9b
zl2dtl <radiodanzl2dtl@gmail.com>
2026-09-22 14:32:12 +1200

    all dec instruction tests written

--- tests/test_cpu.cpp
+++ tests/test_cpu.cpp
@@ -105,6 +105,7 @@ std::cout << "PC before INC: "
 
 void testDec()
 {
+    
     avr::CPU cpu;
 
     // LDI R16, 0x42
@@ -134,11 +135,107 @@ void testDec()
     std::cout << "DEC test passed\n";
 }
 
+bool flagSet(uint8_t sreg, uint8_t flag)
+{
+    return (sreg & flag) != 0;
+}
+void testDecFlags()
+{
+    avr::CPU cpu;
+
+    // LDI R16, 0x80
+    cpu.writeFlash(0, 0x00);
+    cpu.writeFlash(1, 0xE8);
+
+    cpu.step();
+
+    // DEC R16
+    cpu.writeFlash(2, 0x0A);
+    cpu.writeFlash(3, 0x95);
+
+    cpu.step();
+
+    if (cpu.readRegister(16) != 0x7F)
+    {
+        std::cerr << "DEC flags test failed: R16 is not 0x7F\n";
+        std::exit(1);
+    }
+
+    uint8_t sreg = cpu.statusRegister();
+
+    if (!flagSet(sreg, 1 << 3)) // V
+    {
+        std::cerr << "DEC flags test failed: V is not set\n";
+        std::exit(1);
+    }
+
+    if (!flagSet(sreg, 1 << 4)) // S
+    {
+        std::cerr << "DEC flags test failed: S is not set\n";
+        std::exit(1);
+    }
+
+    if (flagSet(sreg, 1 << 2)) // N
+    {
+        std::cerr << "DEC flags test failed: N should be clear\n";
+        std::exit(1);
+    }
+
+    std::cout << "DEC flag test passed\n";
+}
+
+void testDecNegative()
+{
+    avr::CPU cpu;
+
+    // LDI R16, 0x00
+    cpu.writeFlash(0, 0x00);
+    cpu.writeFlash(1, 0xE0);
+
+    cpu.step();
+
+    // DEC R16
+    cpu.writeFlash(2, 0x0A);
+    cpu.writeFlash(3, 0x95);
+
+    cpu.step();
+
+    if (cpu.readRegister(16) != 0xFF)
+    {
+        std::cerr << "DEC negative test failed: R16 is not 0xFF\n";
+        std::exit(1);
+    }
+
+    uint8_t sreg = cpu.statusRegister();
+
+    if (!flagSet(sreg, 1 << 2))
+    {
+        std::cerr << "DEC negative test failed: N is not set\n";
+        std::exit(1);
+    }
+
+    if (!flagSet(sreg, 1 << 4))
+    {
+        std::cerr << "DEC negative test failed: S is not set\n";
+        std::exit(1);
+    }
+
+    if (flagSet(sreg, 1 << 3))
+    {
+        std::cerr << "DEC negative test failed: V should be clear\n";
+        std::exit(1);
+    }
+
+    std::cout << "DEC negative test passed\n";
+}
+
 int main() {
 
     testLdi();
     testMov();
     testInc();
     testDec();
+    testDecFlags();
+    testDecNegative();
     return 0;
 }   
